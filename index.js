8e543588a27cc7f523b162af698bd3faaf06d695
hector <hpulido1021@gmail.com>
2022-03-24 12:12:46 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+ var customerName = "bob";
+ const leastFavoriteCustomer = "const";
+ 
+ function fixTheScop(){
+     return customerName;
+ }
+ 
+
+  function upperCaseCustomerName(){
+    customerName = "BOB";
+    return customerName;
+ } 
+ 
+ function setBestCustomer(){
+      bestCustomer = "not bob";
+      return bestCustomer;
+ }
+
+ function overwriteBestCustomer() {
+     bestCustomer = "maybe bob";
+     return bestCustomer;
+ }
+
+ function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "it is a const";
+    return leastFavoriteCustomer;
+ }
